b3606006378002fcf2936116907a6efcb10e7f19
Craig Donavin <crdonavin@gmail.com>
2019-03-24 16:01:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.insert(1, " and ")
+    array.join
+  elsif array.length > 2
+    array.insert(array.length-1, " and ")
+    array.join(", ")
+    array.delete_at(array.length-1)
+    array
+  else
+    array.join
+  end
 end
