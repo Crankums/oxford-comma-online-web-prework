68c0c1f224836d72876411e84104a547b5aba52d
Craig Donavin <crdonavin@gmail.com>
2019-03-24 16:08:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,6 @@ def oxford_comma(array)
     array.join
   elsif array.length > 2
     array.insert(array.length-1, " and ")
-    array.join(", ")
-    array.delete_at(array.length-1)
-    array
   else
     array.join
   end
